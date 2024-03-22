e671fc8cb6507c9391f7adc10560c94a070ab35f
trytosend <trytosend@centrum.sk>
2024-03-22 08:10:46 +0100

    first commit

---
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tic-tac-toe',
+  standalone: true,
+  imports: [],
+  templateUrl: './tic-tac-toe.component.html',
+  styleUrl: './tic-tac-toe.component.scss'
+})
+export class TicTacToeComponent {
+
+}
